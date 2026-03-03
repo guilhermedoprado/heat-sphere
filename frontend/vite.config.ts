492ccfe54d9c08eb7a9563441cf9fa93c6d446aa
guidoprado <contact@guilhermedoprado.com>
2026-03-03 16:38:33 -0300

    fix: clean nginx config and true allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Isso garante que ele rode no 0.0.0.0
     port: 5173,
     strictPort: true,
+    allowedHosts: true, // Isso desativa a trava de segurança do Vite 6
     watch: {
       usePolling: true,
-    },
-    allowedHosts: [
-      'heatsphere-frontend.whitefield-dc96e279.brazilsouth.azurecontainerapps.io',
-      'heatsphere.guilhermedoprado.com'
-    ]
+    }
   }
 });
